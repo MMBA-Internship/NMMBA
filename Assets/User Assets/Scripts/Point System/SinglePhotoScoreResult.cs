1418ba75c4386e69452e5a63ea675acca47d1861
morqp <mara.cosneanu@yahoo.com>
2026-03-18 16:00:11 +0100

    moved files, started structure of score calculator

---
+++ Assets/User Assets/Scripts/Point System/SinglePhotoScoreResult.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class SinglePhotoScoreResult
+{
+    // class that holds the data(path or texture) of the picture and score
+    public int totalScore;
+    public Texture2D pictureTexture;
+    public string picturePath;
+
+}
