e905166e2df05392c0d21bc9681e643a74723dcc
morqp <mara.cosneanu@yahoo.com>
2026-03-27 15:32:00 +0100

    photo button

--- Assets/User Assets/Scripts/Camera/Picture Taking/PhotoInput.cs
+++ Assets/User Assets/Scripts/Camera/Picture Taking/PhotoInput.cs
@@ -1,30 +1,60 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PhotoInput : MonoBehaviour
 {
-	[SerializeField] private KeyCode photoKey = KeyCode.Space; // ADDED
+    [SerializeField] private KeyCode photoKey = KeyCode.Space;
+    [SerializeField] private float photoCooldown = 3f;
+    bool disableInput = false;
 
-	bool disableInput = false;
+    private void OnEnable()
+    {
+        GameEvents.OnRoundEnded += OnRoundEnded;
+    }
 
-	private void OnEnable()
-	{
-		GameEvents.OnRoundEnded += OnRoundEnded;
-	}
+    private void OnDisable()
+    {
+        GameEvents.OnRoundEnded -= OnRoundEnded;
+    }
 
-	private void OnDisable()
-	{
-		GameEvents.OnRoundEnded -= OnRoundEnded;
-	}
+    private void OnRoundEnded()
+    {
+        disableInput = true;
+    }
 
-	private void OnRoundEnded()
-	{
-		disableInput = true;
-	}
+    public void TakePhoto(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            TakePhoto();
+        }
+    }
 
-	public void TakePhoto(InputAction.CallbackContext ctx)
-	{
-		if (ctx.started && !disableInput)
-			GameEvents.RaisePhotoInputPressed();
-	}
+    private IEnumerator PhotoCooldown_Co()
+    {
+
+        disableInput = true;
+        yield return new WaitForSeconds(photoCooldown);
+        disableInput = false;
+    }
+
+    public void TakePhoto()
+    {
+        Debug.Log("PhotoInput: TakePhoto called");
+        if (!disableInput)
+        {
+            Debug.Log("PhotoInput: TakePhoto called");
+            GameEvents.RaisePhotoInputPressed();
+            StartCoroutine(PhotoCooldown_Co()); ;
+        }
+        Debug.Log("PhotoInput: TakePhoto finished");
+    }
+
+
+    public void debug()
+    {
+        Debug.Log("PhotoInput: debug method called");
+    }
+
 }
