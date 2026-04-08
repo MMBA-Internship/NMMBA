cc90c333eeb80aa721c49b3a54c95ea6f2ad0307
Sno <edialpleijter@gmail.com>
2026-04-08 16:50:45 +0200

    added crab idle anim

--- Assets/User Assets/Art/3d/Animations/animDebug.cs
+++ Assets/User Assets/Art/3d/Animations/animDebug.cs
@@ -12,6 +12,8 @@ public class animDebug : MonoBehaviour
 
     [SerializeField]
     private bool interact1;
+    [SerializeField]
+    private bool interact2;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,45 +31,51 @@ public class animDebug : MonoBehaviour
         if(interact1)
         {
             dbAC.SetTrigger("trInteract1");
-           
+            interact2 = false;
         }
+        else if(interact2)
+ {
+            interact1 = false;
+                dbAC.SetTrigger("trInteract2");
+
+            }
 
 
 
-        /* 
-         // was tryna get the time of the anim playing and tell it to play it again after it's done so the anim doen's tneed to be set to loop but can still loop. however the first time it's triggered it kept grabbing the aniamiton that was playing as the button was pressed. not the new one.
-        // so i instead set it up in the aniamtor to transistion from default swimming to things instead of from any. this way it'l just play a little ibto f swimming realize it should be interacting and flip backl
-        loopInteract1 = interact1;
+            /* 
+             // was tryna get the time of the anim playing and tell it to play it again after it's done so the anim doen's tneed to be set to loop but can still loop. however the first time it's triggered it kept grabbing the aniamiton that was playing as the button was pressed. not the new one.
+            // so i instead set it up in the aniamtor to transistion from default swimming to things instead of from any. this way it'l just play a little ibto f swimming realize it should be interacting and flip backl
+            loopInteract1 = interact1;
 
-    
-        if (loopInteract1 && interactLength <= 0)
-        {
-            dbAC.SetTrigger("trInteract1");
-            loopInteract1 = false;
 
-        
+            if (loopInteract1 && interactLength <= 0)
+            {
+                dbAC.SetTrigger("trInteract1");
+                loopInteract1 = false;
 
-                //thank you so much for helping me with getting the animation time anca
-                AnimatorStateInfo stateInfo = dbAC.GetCurrentAnimatorStateInfo(0);
-                interactLength = stateInfo.length;
-                Debug.Log("Lenght of animation = " + interactLength);
- 
+
+
+                    //thank you so much for helping me with getting the animation time anca
+                    AnimatorStateInfo stateInfo = dbAC.GetCurrentAnimatorStateInfo(0);
+                    interactLength = stateInfo.length;
+                    Debug.Log("Lenght of animation = " + interactLength);
+
+            }
+
+            if (interactLength > 0)
+            {
+                interactLength -= Time.deltaTime;
+                //Debug.Log("Anim play cooldown = " + interactLength);
+            }
+
+
+
+            if (dbAC == null)
+            {
+                Debug.LogWarning("no animation controller assighned on " + this.gameObject.name);
+                return;
+
+            }
+            */
         }
-
-        if (interactLength > 0)
-        {
-            interactLength -= Time.deltaTime;
-            //Debug.Log("Anim play cooldown = " + interactLength);
-        }
-        
-       
-
-        if (dbAC == null)
-        {
-            Debug.LogWarning("no animation controller assighned on " + this.gameObject.name);
-            return;
-        
-        }
-        */
-    }
 }
